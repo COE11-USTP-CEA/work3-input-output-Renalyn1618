3ccb7c9ac3dbf9b7f607352c0a05d881d7e8dbd1
Renalyn S. Duron <duronrenalyn@gmail.com>
2017-01-27 21:57:16 +0800

    Create project3io.java

---
+++ project3io.java
@@ -0,0 +1,9 @@
+class project3io{
+    
+    public static void main(String[]args){
+        for(String j: args){
+            System.out.println("Hello " + j );
+        }
+        System.out.println("There are " + args.length + " arguments");
+    }
+}
